b18f8e97060fb0e68bf7246088a9ad70442f273e
Firegamer4444 <147237350+Firegamer4444@users.noreply.github.com>
2024-05-13 13:54:14 +0100

    initial commit

---
+++ src/CuentaBancaria.java
@@ -0,0 +1,32 @@
+public class CuentaBancaria {
+    private String ccc;
+    private String dni;
+    private String nombreTitular;
+    private String saldoActual; 
+
+    public CuentaBancaria(){
+    }
+
+    public String getNumeroCuenta(){
+        String numeroCuenta = this.ccc.substring(9 , 19);
+        return numeroCuenta;
+    }
+
+    public String getEntidad(){
+        String entidad = this.ccc.substring(0 , 4);
+        return entidad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombreTitular() {
+        return nombreTitular;
+    }
+
+    public String getSaldoActual() {
+        return saldoActual;
+    }
+
+}
